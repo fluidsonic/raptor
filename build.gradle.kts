32e3465976399308e6f4dfb2572720f01d62dc05
Marc Knaup <marc@knaup.io>
2021-02-08 03:44:32 +0100

    🎉 0.9.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ allprojects {
 	}
 }
 
-fluidLibrary(name = "raptor", version = "0.9.4-kotlin-1.5", prefixName = false) {
+fluidLibrary(name = "raptor", version = "0.9.5", prefixName = false) {
 	allModules {
 		language {
 			withExperimentalApi("io.fluidsonic.raptor.RaptorInternalApi")
