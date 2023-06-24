6d11b9975705876328efedcbb65fb069bba38be8
Marc Knaup <marc@knaup.io>
2023-06-24 11:22:51 +0700

    Updated all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import io.fluidsonic.gradle.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.2.2"
+	id("io.fluidsonic.gradle") version "1.3.2"
 }
 
 fluidLibrary(name = "raptor", version = "0.17.24", prefixName = false) {
