f07ea370a1c43c5617d2fb663c0a6f09c25feb67
Marc Knaup <marc@knaup.io>
2025-10-21 05:02:08 +0700

    fluid-gradle 2.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import io.fluidsonic.gradle.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "2.0.1"
+	id("io.fluidsonic.gradle") version "2.0.2"
 }
 
 fluidLibrary(name = "raptor", version = "0.29.0", prefixName = false) {
